826254c52ac06e4029cbc402c99de202e2ed60d2
stakenschneider <volkova.maria.d@gmail.com>
2014-12-29 15:53:13 +0300

    Update kr4.cpp

--- kr4.cpp
+++ kr4.cpp
@@ -16,43 +16,37 @@ int main(void)
         return -1;
     }
 
-while (!fin.eof())
-	{
+        while (!fin.eof())
+    {
 		getline(fin,str);
 		j++;
-	}
+    }
 
-j*=2;
-string *mass = new string[j];
-
-fin.seekg(0);
-for(int i=0; i<j; i++)
-{
+        j*=2;
+        string *mass = new string[j];
+        fin.seekg(0);
+        for(int i=0; i<j; i++)
 	fin>>mass[i];
-}
 
-
-
- for(int i=1; i<j; i=i+2)
-    for(int q=i+2; q<j; q=q+2)
-    if(mass[q]<mass[i])
-	{
-		swap(mass[i],mass[q]);
+        for(int i=1; i<j; i=i+2)
+        for(int q=i+2; q<j; q=q+2)
+        if(mass[q]<mass[i])
+    {
+	swap(mass[i],mass[q]);
         swap(mass[i-1],mass[q-1]);
-	} else if(mass[q]==mass[i])
-	{
-		if  (mass[i-1]>mass[q-1]){
-			swap(mass[i],mass[q]);
-			swap(mass[i-1],mass[q-1]);
+    } else if(mass[q]==mass[i])
+    {  if  (mass[i-1]>mass[q-1])
+               {      
+               	   swap(mass[i],mass[q]);
+		   swap(mass[i-1],mass[q-1]);
 		}
-	}
+    }
 
- for(int i=0; i<j-1; i=i+2)
-    fout<<mass[i]<<" "<<mass[i+1]<<"\n";
+        for(int i=0; i<j-1; i=i+2)
+        fout<<mass[i]<<" "<<mass[i+1]<<"\n";
 
- delete[]mass;
+        delete[]mass;
 	fin.close();
 	fout.close();
-	system ("pause");
-return 0;
+        return 0;
 }
