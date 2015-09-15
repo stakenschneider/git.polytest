0227bdeab88da72d53b2b0e90550f16f3f495d38
Masha <volkova.maria.d@gmail.com>
2015-09-15 21:44:29 +0300

    Update test1.cpp

--- test1.cpp
+++ test1.cpp
@@ -1,11 +1,12 @@
 //volkova maria 13501/3
 //вычисление квадратных корней уравнения
-
-
-
 #define _MATH_USE_DEFINE
 #include<iostream>
 #include<cmath>
+#include<math.h>
+
+
+
 using namespace std;
 int main(void)
 {
@@ -13,22 +14,44 @@ int main(void)
     int a, b, d, c, x1, x2;
     
     cout << "Введите коэффициенты квадратного уравнения:\n";
-    cin >>a>>b>>c;
+    cin >> a >> b >> c;
     cout << "\n";
-    d=sqrt(b*b-4*a*c);   //считаем дискриминант
+    d = sqrt( b*b-4*a*c );   //дискриминант
  
-    //далее стандартный алгоритм поиска корней
-    
+    //стандартный алгоритм поиска корней  
     if (d==0)                       
-    {  x1=(-b)/(2*a);
-       cout <<"x: "<< x1 <<endl; }
+    {  
+		x1 = (-b)/(2*a);
+       cout << "Дискриминант\n" << "x: "<< x1 << endl; 
+	}
     
     if (d>0)
-    {  x1=(-b+d)/(2*a);
-       x2=(-b-d)/(2*a);
-       cout <<"x1:"<<x1<<"  x2:" <<x2<< endl; }
+    { 
+	   x1 = (-b+d)/(2*a);
+       x2 = (-b-d)/(2*a);
+       cout << "Дискриминант\n" << "x1: " << x1 << "  x2: " << x2 << endl; 
+	}
     
-    if  (d<0) { cout<<"Квадратных корней нет\n\n"; }
-    
+    if  (d<0) { cout << "Квадратных корней нет\n\n"; }
+	
+
+
+	//алкоритм Виета
+	for(double i=-(abs(a)+abs(b)+abs(c)); i<=abs(a)+abs(b)+abs(c) ; i+=1)
+      {
+         for(double j=i; j<=abs(a)+abs(b)+abs(c); j+=1)
+         {
+            if(i+j==b && i*j==c)
+            {
+                x1=j;
+                x2=i;
+                cout << "Виета\n"<< "x1: " << x1 << "  x2: " << x2 << endl; 
+            }
+        }
+      }
+
+
+
+    system("pause");
     return 0;
 }
