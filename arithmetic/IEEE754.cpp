859ec87d50902b6ecad64e1a1606358a0cbf6bde
Masha <volkova.maria.d@gmail.com>
2015-10-30 02:51:00 +0300

    Update IEEE754.cpp

--- arithmetic/IEEE754.cpp
+++ arithmetic/IEEE754.cpp
@@ -9,13 +9,15 @@
 #include <bitset>
 
 
+// IEEE754 Double precision 64-bit
+
 int main(void)
 {
     double d ;
-    std::cout << "Input: ";
-    std::cin >> d ;
-    unsigned long long *i = reinterpret_cast<unsigned long long *>(&d);
-    std::cout << "OutputOutput: " << std::bitset<64>(*i) << std::endl;
-    std::cout << "первая цифра знак числа (о если +, 1 если -)\nследущие 8 бит - смещенная значение\n23 бита - дробная часть мантисы";
+    std :: cout << "Input: ";
+    std :: cin >> d ;
+    unsigned long long *i = reinterpret_cast < unsigned long long *> (&d);
+    std :: cout << "Output: " << std :: bitset <64> (*i) << std :: endl;
+    std :: cout << "первая цифра знак числа (о если +, 1 если -)\nследущие 11 цифр - экспонента\nвсе остальные цифры - мантиса";
     return 0;
 }
