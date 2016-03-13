62d02784f3757214c984b82ba606b6347c9aef18
Masha <volkova.maria.d@gmail.com>
2016-03-13 03:24:02 +0300

    Update back_to_school.pas

--- back_to_school.pas
+++ back_to_school.pas
@@ -1,5 +1,3 @@
-{привет тимур и это говно мы никогда не сдадим :3 }
-
 Uses CRT;
 var
 life:array[1..100,1..100] of integer;
@@ -12,33 +10,30 @@ for x:=1 to 26 do
 for y:=1 to 26 do
 life[x,y]:=0;
 
-
 ReadLn();
 ClrScr;
 x:=50;
 y:=50;
-while (x<>0) or (y<>0)   do begin
+
+while (x<>0) or (y<>0) do begin
 life[x,y]:=1;
 Write('введите  x : ');
 Read(x);
 if x<>0 then
 x:=x+1;
- Write('введите  y : ');
+Write('введите  y : ');
 Read(y);
-if y<>0 then
-y:=y+1;
-end;
+if y<>0 then y:=y+1; end;
+
 ClrScr;
 n:=1;
 textcolor(2);
+
 for x:=2 to 25 do begin
 for y:=2 to 25 do
-if life[x,y]=1 then
-write(#254)
-else
-write(#255);
-writeln;
-end;
+if life[x,y]=1 then write(#254)
+else write(#255); writeln; end;
+
 while n<>0 do begin
 n:=0;
 for x:=2 to 25 do
@@ -62,9 +57,7 @@ if (life[x-1,y-1]=1) or (life[x-1,y-1]=2) then
 l:=l+1;
 if ((l>3) or (l<2)) and (life[x,y]=1) then
 life[x,y]:=2;
-L:=0;
-end;
- 
+L:=0; end;
  
 for x:=2 to 25 do
 for y:=2 to 25 do begin
@@ -87,36 +80,32 @@ if (life[x-1,y-1]=1) or (life[x-1,y-1]=2) then
 l:=l+1;
 if (l=3) and (life[x,y]<>1) then
 life[x,y]:=3;
-L:=0;
-end;
+L:=0; end;
+
 for x:=2 to 25 do
 for y:=2 to 25 do begin
-if life[x,y]=2 then
-life[x,y]:=0;
-end;
+if life[x,y]=2 then life[x,y]:=0; end;
+
 for x:=2 to 25 do
 for y:=2 to 25 do begin
-if life[x,y]=3 then
-life[x,y]:=1;
-end;
+if life[x,y]=3 then life[x,y]:=1; end;
+
 delay(1000);
 ClrScr;
+
 for x:=2 to 25 do begin
 for y:=2 to 25 do
-if life[x,y]=1 then
-write(#254)
-else
-write(#255);
-writeln;
-end;
+if life[x,y]=1 then write(#254)
+else write(#255); writeln; end;
+
 n:=0;
 for x:=2 to 25 do begin
 for y:=2 to 25 do
-if life[x,y]=1 then
-n:=n+1;
-end;
+if life[x,y]=1 then n:=n+1; end;
+
 write('kol-vo shivih lketok ',N);
 end;
+
 ClrScr;
 WriteLn('Life End');
 ReadLn(i);
